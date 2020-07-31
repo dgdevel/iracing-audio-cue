e673bf5333e3c5d876d691c21543e4185d19da82
dgdevel <daniele.guttuso@protonmail.com>
2020-07-31 17:52:28 +0200

    clear signal for leftright

--- main.py
+++ main.py
@@ -42,23 +42,31 @@ def driver_brief(ir, caridx):
 
 class LeftRightState:
 	lastplay = 0
+	canclear = False
 
 def leftright_handler(ir, config, state):
 	current_time = time.time()
+	if state.canclear and  not (ir['CarLeftRight'] == irsdk.CarLeftRight.car_left or ir['CarLeftRight'] == irsdk.CarLeftRight.two_cars_left or ir['CarLeftRight'] == irsdk.CarLeftRight.car_right or ir['CarLeftRight'] == irsdk.CarLeftRight.two_cars_right or ir['CarLeftRight'] == irsdk.CarLeftRight.car_left_right):
+		state.canclear = False
+		print('clear!')
+		play('leftright_clear')
 	if (current_time - state.lastplay) < float(config['repeat_interval']):
 		return
 	if ir['CarLeftRight'] == irsdk.CarLeftRight.car_left or ir['CarLeftRight'] == irsdk.CarLeftRight.two_cars_left:
 		print('leftright left')
 		play('leftright_left')
 		state.lastplay = current_time
+		state.canclear = True
 	elif ir['CarLeftRight'] == irsdk.CarLeftRight.car_right or ir['CarLeftRight'] == irsdk.CarLeftRight.two_cars_right:
 		print('leftright right')
 		play('leftright_right')
 		state.lastplay = current_time
+		state.canclear = True
 	elif ir['CarLeftRight'] == irsdk.CarLeftRight.car_left_right:
 		print('leftright both')
 		play('leftright_both')
 		state.lastplay = current_time
+		state.canclear = True
 
 class FastClassBehindState:
 	pcts = {}
@@ -71,10 +79,13 @@ def fastclassbehind_delta(current_pct, pct):
 	return delta * 100.0
 
 def fastclassbehind_handler(ir, config, state):
+	warn_repeat_after = float(config['warn_repeat_after'])
+	current_time = time.time()
+	if state.lastplay + warn_repeat_after < current_time:
+		return
 	average_lap_duration = ir['DriverInfo']['DriverCarEstLapTime']
 	warn_threshold = float(config['warn_threshold'])
 	warn_threshold_min = float(config['warn_threshold_min'])
-	warn_repeat_after = float(config['warn_repeat_after'])
 	pct_delta = warn_threshold * 100.0 / average_lap_duration
 	pct_delta_min = warn_threshold_min * 100.0 / average_lap_duration
 	current_caridx = ir['CamCarIdx']
@@ -89,12 +100,10 @@ def fastclassbehind_handler(ir, config, state):
 					state.pcts[caridx] = pct
 					delta = fastclassbehind_delta(current_pct, pct)
 					if delta > pct_delta_min and delta < pct_delta:
-						current_time = time.time()
-						if state.lastplay + warn_repeat_after < current_time:
-							print("fast car behind " + driver_brief(ir, caridx) + " delta = " + str(delta) + " me=" + str(current_pct) + " other=" + str(pct))
-							play('fastclassbehind')
-							state.lastplay = current_time
-							return
+						print("fast car behind " + driver_brief(ir, caridx) + " delta = " + str(delta) + " me=" + str(current_pct) + " other=" + str(pct))
+						play('fastclassbehind')
+						state.lastplay = current_time
+						return
 
 def main_thread():
 	config = configparser.ConfigParser()
